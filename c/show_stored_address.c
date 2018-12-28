336195173e2233351bd043aed599b8b9aedaa675
forte916 <fortissimo916@gmail.com>
2018-12-28 15:20:44 +0900

    Show address of bss segment

--- c/show_stored_address.c
+++ c/show_stored_address.c
@@ -10,6 +10,11 @@
 char global_str[] = "global string";
 static char static_global_str[] = "static global string";
 
+const char const_str[] = "const string";
+static const char static_const_str[] = "static const string";
+
+char bss_str[];
+
 void show_stored_address()
 {
 	char local_str[] = "local string";
@@ -27,8 +32,12 @@ void show_stored_address()
 	printf("%p: %s \n", global_str, global_str);
 	printf("%p: %s \n", static_global_str, static_global_str);
 
-	printf("%p: %s \n", malloc_str, malloc_str);
+	printf("%p: %s \n", const_str, "const string");
+	printf("%p: %s \n", static_const_str, "static const string");
+
 	printf("%p: %s \n", "literal string", "literal string");
+	printf("%p: %s \n", bss_str, "bss string");
+	printf("%p: %s \n", malloc_str, malloc_str);
 }
 
 
@@ -42,17 +51,19 @@ int main(int argc, char *argv[])
 /*
 Console output:
 
-$ gcc -o show_stored_address show_stored_address.c
-$ ./show_stored_address
+// On cygwin in Windows
 $ ./show_stored_address
 Hello, world!
 0xffffcbbb: local string
 0x100402040: static local string
 0x100402010: global string
 0x100402020: static global string
+0x100403000: const string
+0x100403010: static const string
+0x10040304e: literal string
+0x1004071a0: bss string
 0x600000440: malloc string
-0x100403009: literal string
 
-The variables are stored in one of stack, static(data segment) or heap regions
+The variables are stored in one of stack, static(data segment), bss segment or heap regions
 
 */
